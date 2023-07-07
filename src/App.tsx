3e0c3ef8dacb8e6b901834d5c3fa7746e5d7bab3
Felipe Duarte <feduarte.dev@gmail.com>
2023-07-07 11:18:23 -0300

    primeiro requisito pronto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 function App() {
+  function upperCase(e: React.ChangeEvent<HTMLInputElement>) {
+    e.target.value = (`${e.target.value}`).toUpperCase();
+  }
+
   return (
-    <h1>
-      React Form
-    </h1>
+    <form>
+      <label>
+        Nome
+        <input
+          maxLength={ 40 }
+          type="text"
+          onInput={ upperCase }
+        />
+      </label>
+      <label>
+        Email
+        <input maxLength={ 50 } type="email" />
+      </label>
+      <label>
+        CPF
+        <input maxLength={ 11 } type="text" />
+      </label>
+    </form>
   );
 }
 
